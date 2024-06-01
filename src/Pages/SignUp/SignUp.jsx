799cb430a27331af4b8ecc542aa641efac8d8888
Robiul@Hasan <robihasan093@gmail.com>
2024-06-02 01:22:39 +0600

    emplement react hook form and firebase signUp funcationality

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,6 +1,35 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const SignUp = () => {
+
+    const {createUser, aj} = useContext(AuthContext);
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm()
+
+      const handelSinUp = (data) => {
+          const name = data.name;
+          const email = data.email;
+          const password = data.password;
+          console.log(aj, name, email, password)
+
+          createUser(email, password)
+          .then(res =>{
+            console.log(res.user);
+          })
+          .catch(err =>{
+            console.log(err);
+          })
+      }
+
+    
+    
     return (
         // <div className="hero min-h-screen bg-base-200">
         //     <div className="hero-content flex-col lg:flex-row-reverse">
@@ -10,30 +39,30 @@ const SignUp = () => {
                 <h2 className="text-4xl font-semibold text-[#3C3C3C] mb-6">Sign Up</h2>
                 <p className="text-2xl text-[#7F7F7F] font-medium mb-4">Create Your Account</p>
             </div>
-            <form className="card-body">
+            <form onSubmit={handleSubmit(handelSinUp)} className="card-body">
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Name</span>
                     </label>
-                    <input type="text" placeholder="Your Name" className="input input-bordered" required />
+                    <input type="text" {...register("name", { required: true })} placeholder="Your Name" className="input input-bordered" required />
                 </div>
                 {/* <div className="form-control">
                             <label className="label">
                                 <span className="label-text">ImageURL</span>
                             </label>
-                            <input type="text" placeholder="ImageURL" className="input input-bordered" required />
+                            <input type="text" {...register("imageURL", { required: true })} placeholder="ImageURL" className="input input-bordered" required />
                         </div> */}
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Email</span>
                     </label>
-                    <input type="email" placeholder="email" className="input input-bordered" required />
+                    <input type="email" {...register("email", { required: true })} placeholder="email" className="input input-bordered" required />
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Password</span>
                     </label>
-                    <input type="password" placeholder="password" className="input input-bordered" required />
+                    <input type="password" {...register("password", { required: true })} placeholder="password" className="input input-bordered" required />
                     {/* <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label> */}
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -2,16 +2,20 @@ import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { createContext } from "react";
 import { app } from "../../firebaseConfig";
 
-export const AuthContext = createContext(null)
+export const AuthContext = createContext({});
+
 const AuthProvider = ({children}) => {
 
+    const aj = 'hi'
     const auth = getAuth(app);
     const createUser = (email, password) =>{
-        return createUserWithEmailAndPassword(email, password);
+        console.log(email);
+        return createUserWithEmailAndPassword(auth, email, password);
     }
-    
+
     const authInfo = {
         createUser,
+        aj,
     }
     return (
         <AuthContext.Provider value={authInfo}>
